73a99ecedef956d77170a5dc82916d43547639dd
Wes Bailey <espressoandcode@gmail.com>
2019-12-28 14:19:12 -0500

    Modify input() to work for Python 2 or 3

--- ezkeyvault.py
+++ ezkeyvault.py
@@ -1,8 +1,10 @@
 import os
+import sys
 import json
 from datetime import datetime
 
 def main():
+
     # Get user home path and create full filepath
     home = os.path.expanduser("~") + '/'
     fn = os.path.join(home, '.keyvault.json')
@@ -10,14 +12,21 @@ def main():
     updated_keyfile = {}
     dash = '-' * 100
 
+    # Modify input() to work for Python 2 or 3
+    global input
+    try:
+        input = raw_input
+    except NameError:
+        pass
+
     print('              __ __          _    __            ____')
     print('  ___  ____  / //_/__  __  _| |  / /___ ___  __/ / /_')
     print(' / _ \/_  / / ,< / _ \/ / / / | / / __ `/ / / / / __/')
     print('/  __/ / /_/ /| /  __/ /_/ /| |/ / /_/ / /_/ / / /_  ')
     print('\___/ /___/_/ |_\___/\__, / |___/\__,_/\__,_/_/\__/ ')
-    print('                    /____/')
+    print('                    /____/\n')
 
-    # If the 'apivault.txt' file exists, load it
+    # If the 'keyvault.json' file exists, load it
     if (os.path.exists(fn)):
         with open(fn, 'r') as f:
             # Guard against empty file
@@ -26,27 +35,25 @@ def main():
                 updated_keyfile = json.loads(file)
 
         print(dash)
-        print("KEYNAME             KEY (first 16 digits)       TIMESTAMP                         DOCS")
+        print("KEYNAME             KEY (first 16 digits)       TIMESTAMP                         COMMENT")
         print(dash)
         for k, v in updated_keyfile.items():
-            print('{:<20}{:>20}{:>20}{:>40}'.format(k, v['value'][:16], v['date_created'], v['docs_site']))
-        print('\n\n')
+            print('{:<20}{:>20}{:>20}{:>40}'.format(k, v['value'][:16], v['date_created'], v['comment']))
+        print('\n')
 
     running = True
     while running:
         key = input("Enter the new API key name: ")
         val = input("Enter the new API key value: ")
-        site = input("Enter a URL for documentation (optional)")
+        comment = input("Enter an optional comment: ")
         confirm = input("Add this key to the vault? (Y/N)")
         timestamp = datetime.now().strftime("%H:%M:%S.%f")
 
         newkey = {
-            "id": "",
             "value": val,
-            "date_created": timestamp
+            "date_created": timestamp,
+            "comment": comment
         }
-        if not site == None:
-            newkey["docs_site"] = site
 
         if confirm == 'Y' or confirm == 'y':
             if key in updated_keyfile.keys():
@@ -56,15 +63,14 @@ def main():
 
             if writeflag == 'Y' or writeflag == 'y':
                 updated_keyfile[key] = newkey
-                print("Key for " + key + " has been updated.")
-                print('Updated keyfile: ', updated_keyfile)
+                print("Key for " + key + " has been updated.\n")
                 with open(fn, 'w') as writefile:
                     writefile.write(json.dumps(updated_keyfile))
             else:
-                print("New key for "+ key +" has been discarded.")
+                print("New key for "+ key +" has been discarded.\n")
 
         else:
-            print("New key for " + key + " has been discarded.")
+            print("New key for " + key + " has been discarded.\n")
 
         keep_running = input("Would you like to add another key to the vault? (Y/N)")
         running = True if (keep_running == 'Y' or keep_running == 'y') else False
